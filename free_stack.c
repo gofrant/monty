06283abcf0cf936b50ce273188d34c0896fbaccb
gofrant <gofran178@gmail.com>
2023-01-22 12:16:39 -0800

    ggt

---
+++ free_stack.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ *  free_stack - frees a doubly linked lis
+ *  @head: head of the stack
+ */
+void free_stack(stack_t *head)
+{
+	stack_t *aux;
+
+	aux = head;
+
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+
+	}
+}
